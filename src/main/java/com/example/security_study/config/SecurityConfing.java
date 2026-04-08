38df039449fbd2b9de9dcd44257ef019809d6386
문정훈 <mjh090801abc@gmail.com>
2026-04-08 17:50:39 +0900

    시큐리티 설정에 대한 어노테이션과 함수에 대한 주석 추가

--- src/main/java/com/example/security_study/config/SecurityConfing.java
+++ src/main/java/com/example/security_study/config/SecurityConfing.java
@@ -7,7 +7,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // 스프링의 설정 정보(Bean 정의)를 담고 있음
 public class SecurityConfing {
-    @Bean
+    @Bean// 이 메서드가 생성해서 반환하는 객체를 스프링 컨테이너가 관리하는 공용 객체(Bean)로 등록
+    // SecurityFilterChain : 반환 타입(보안 필터 체인이라는 객체를 반환)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
     }
